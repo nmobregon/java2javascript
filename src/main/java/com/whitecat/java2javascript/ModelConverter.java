7ee6fea92aa56b0894e43c52e48c082a4cd261e6
Nicolas Obregon <nmobregon@gmail.com>
2015-09-28 00:17:14 -0300

    Simple model conversion

---
+++ src/main/java/com/whitecat/java2javascript/ModelConverter.java
@@ -0,0 +1,56 @@
+package com.whitecat.java2javascript;
+public class ModelConverter{
+
+     public static void main(String []args){
+        
+        String appName = "app";
+        
+        try{
+            Class[] clazzes = {Paciente.class};
+            
+            for (Class clazz : clazzes){
+                StringBuffer metadataBuffer = new StringBuffer();
+                
+                System.out.println(appName+".namespace('"+appName+".model."+clazz.getSimpleName()+"');");
+                System.out.println(appName+".model."+clazz.getSimpleName()+" = function(dataObject) {");
+                System.out.println("\tif(!(this instanceof "+appName+".model."+clazz.getSimpleName()+")) {");
+                System.out.println("\t\treturn new "+appName+".model."+clazz.getSimpleName()+"(dataObject);");
+                System.out.println("\t}");
+                
+                System.out.println("\tvar data = dataObject || {};");
+    
+                for (java.lang.reflect.Method me : clazz.getDeclaredMethods()){
+                    if (me.getName().startsWith("get")){
+                        String propName = me.getName().replace("get", "").substring(0,1).toLowerCase() + 
+                                            me.getName().replace("get", "").substring(1);
+                        
+                        String type = me.getReturnType().getName().toLowerCase().substring(0,3).equals("int") ? "INTEGER" : "TEXT";
+                        
+                        System.out.println("\tthis." + propName+"= data."+propName+" || null;");
+                        
+                        metadataBuffer.append("\t\t\t{ name: '"+propName+"', type: '"+type+"' },\n");
+                        
+                    }
+                }
+                System.out.println("};");
+                
+                
+                
+                System.out.println(appName+".model."+clazz.getSimpleName()+".getMetadata = function() {");
+                System.out.println("\treturn {");
+                System.out.println("\t\tname: "+clazz.getSimpleName().toLowerCase()+",");
+                System.out.println("\t\tattributes: [");
+                System.out.println(metadataBuffer );
+                System.out.println("\t\t]");
+                System.out.println("\t};");
+                System.out.println("};");    
+            }
+            
+        }
+        catch(Exception e){
+            System.out.println(e);    
+        }
+       
+        
+     }
+}
